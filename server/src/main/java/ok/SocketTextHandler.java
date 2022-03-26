51daef178d9fb476d117186743ab2024c9d9672c
Oleks K <oleks.korshak@gmail.com>
2022-03-26 12:17:57 -0700

    change text

--- server/src/main/java/ok/SocketTextHandler.java
+++ server/src/main/java/ok/SocketTextHandler.java
@@ -18,18 +18,18 @@ public class SocketTextHandler extends TextWebSocketHandler {
 
 		String payload = message.getPayload();
 //		JSONObject jsonObject = new JSONObject(payload);
-		session.sendMessage(new TextMessage("Hi " + payload + " how may we help you?"));
+		session.sendMessage(new TextMessage("Hi " + payload + " how may we help you? asdf"));
 	}
 
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
 		System.err.println("established connection");
-		session.sendMessage(new TextMessage("hello"));
+		session.sendMessage(new TextMessage("hello asdf"));
 	}
 
 	@Override
 	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-		System.err.println("afterConnectionClosed");
+		System.err.println("afterConnectionClosed asdf");
 	}
 	
 	
